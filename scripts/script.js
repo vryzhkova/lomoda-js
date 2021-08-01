e74f8c1e9d1c690304ecc160b52409de85feb715
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2021-08-01 11:53:56 +0300

    Добавляет модальное окно и блокировщик скролла

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,55 @@ headerCityButton.addEventListener('click', () => {
     localStorage.setItem('lomoda-location', city)
 });
 
+// Блокировка скролла
+
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+    document.body.dbScrollY = window.scrollY;
+
+    document.body.style.cssText = `
+        position: fixed;
+        top: ${-windoww.scrollY}px;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        padding-right: ${widthScroll}px;
+    `;
+};
+
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY
+    })
+};
+
 // Модальное окно
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cardModalOpen =  () => {
+    cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
+};
+
+const cardModalClose =  () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+    enableScroll();
+};
+
+subheaderCart.addEventListener('click', cardModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cardModalClose();
+    }
+    
+});
+
 
